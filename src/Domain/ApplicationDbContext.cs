53df32fc584b682cf971a5b03ffddda99298c904
mahilioutsau.i <mahilioutsau.i@MAHILIOUTSAU>
2016-11-29 13:29:06 +0300

    Add project files.

---
+++ src/Domain/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Domain.Entities;
+
+namespace Domain
+{
+    public class ApplicationUser : IdentityUser { };
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json").Build().GetSection("ConnectionString")["DefaultConnection"];
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        DbSet<Manufactorer> Manufatorers;
+    }
+}
