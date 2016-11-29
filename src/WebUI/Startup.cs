79f73b82b288b3b458edded3f87fef6ea49816d0
mahilioutsau.i <mahilioutsau.i@MAHILIOUTSAU>
2016-11-29 16:03:59 +0300

    First page

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -13,14 +13,13 @@ namespace WebUI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(ServiceLifetime.Scoped);
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -30,10 +29,16 @@ namespace WebUI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller}/{action}/{id?}",
+                        defaults: new { controller = "Home", action = "Hello" }
+                    );
+                }
+            );
+
         }
     }
 }
