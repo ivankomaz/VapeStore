49ae8c5752c1726133382947faa1f413b25a6ed7
mahilioutsau.i <mahilioutsau.i@MAHILIOUTSAU>
2016-11-30 17:48:10 +0300

    x

---
+++ src/Domain/Entities/EMod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EMod
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int BatteryType { get; set; }
+        public int MaxPower { get; set; }
+
+        public  int ManufactorerId { get; set;  }
+        public Manufactorer Manufactorer { get; set; }
+    }
+}
--- src/Domain/Entities/Manufactorer.cs
+++ src/Domain/Entities/Manufactorer.cs
@@ -9,5 +9,6 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
     }
 }
